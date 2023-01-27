65e80715c26bbd7eed29c59f36e799fd35ce1816
Dom <domadiyanirav210@gmail.com>
2023-01-27 11:03:14 +0530

    New changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webbrowser
 import pyautogui as pg
 import time
+from urllib.parse import quote
 
 numbers = []
 f = open("numbers.txt", "r")
@@ -8,8 +9,13 @@ for line in f.read().splitlines():
 	if line.strip() != "":
 		numbers.append(line.strip())
 f.close()
+
+f = open("message.txt", "r")
+message = quote(f.read())
+f.close()
+
 for num in numbers:
-    webbrowser.open(f'https://web.whatsapp.com/send?phone={num}&text=Hey!')
+    webbrowser.open(f'https://web.whatsapp.com/send?phone={num}&text={message}')
     time.sleep(8)
     pg.press('enter')
     time.sleep(5)
